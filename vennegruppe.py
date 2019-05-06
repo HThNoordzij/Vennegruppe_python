3489b95c1555bfcb11eda1eef5cdab199dab79a4
HThNoordzij <hannanoordzij@gmail.com>
2019-05-06 11:49:19 +0200

    Spell and PEP8 check

--- vennegruppe.py
+++ vennegruppe.py
@@ -2,7 +2,7 @@
 #                                                               #
 #     The user should use the provided template excel file.     #
 #                                                               #
-#     !! IMPORTANT, the archief sheet (Arkiv) should contain    #
+#     !! IMPORTANT, the archive sheet (Arkiv) should contain    #
 #     all the children currently in the class. This list will   #
 #     be used to make the new friend groups                     #
 #                                                               #
@@ -13,7 +13,6 @@ import math
 import sys
 import random
 from openpyxl import load_workbook
-
 
 #################################################################
 #                                                               #
@@ -27,19 +26,16 @@ if len(sys.argv) != 2:
     print("Usage: python vennegruppe.py infile")
     sys.exit(1)
 
-
 # Open infile
 print("Opening infile")
 infile = sys.argv[1]
 wb_infile = load_workbook(filename=infile, read_only=True)
-
 
 # Check if the provided set-up excel file is used by checking sheet names
 sheet1, sheet2, sheet_hidden = wb_infile.sheetnames
 if not sheet1 == 'Vennegruppe' or not sheet2 == 'Arkiv':
     print("Please use the provided example file")
     sys.exit(1)
-
 
 #################################################################
 #                                                               #
@@ -51,19 +47,17 @@ if not sheet1 == 'Vennegruppe' or not sheet2 == 'Arkiv':
 # grab the groups worksheet (Vennegruppe)
 sheet1_infile = wb_infile["Vennegruppe"]
 
-
-# Initialize dictionary to hold the previous friendgroups (Vennegruppe)
+# Initialize dictionary to hold the previous friend groups (Vennegruppe)
 groups = {}
 number_of_groups = 0
 number_of_kids_in_group = 0
 
-
-# grab the latest friendgroups (Vennegruppe)
-print("\nReading latest friendgroups")
+# grab the latest friend groups (Vennegruppe)
+print("\nReading latest friend groups")
 while True:
     number_of_groups += 1
 
-    if not sheet1_infile.cell(row = 1, column = number_of_groups).value:
+    if not sheet1_infile.cell(row=1, column=number_of_groups).value:
         break
 
     groups[number_of_groups] = list()
@@ -72,12 +66,12 @@ while True:
     while True:
         number_of_kids_in_group += 1
 
-        if not sheet1_infile.cell(row = (number_of_kids_in_group + 1), column = number_of_groups).value:
+        if not sheet1_infile.cell(row=(number_of_kids_in_group + 1), column=number_of_groups).value:
             number_of_kids_in_group = 0
             break
 
-        groups[number_of_groups].append(sheet1_infile.cell(row = (number_of_kids_in_group + 1), column = number_of_groups).value)
-        
+        groups[number_of_groups].append(sheet1_infile.cell(row=(number_of_kids_in_group + 1),
+                                                           column=number_of_groups).value)
 
 #################################################################
 #                                                               #
@@ -89,14 +83,12 @@ while True:
 # Change to archive sheet (Arkiv)
 sheet2_infile = wb_infile["Arkiv"]
 
-
 # Initialize variables to store 'who_has_been_in_a_group_with'
-archief = {}
+archive = {}
 child = 0
 child_name = ""
 has_been_in_group_with = list()
 iterator = 3
-
 
 # grab the name and gender of all the children (Kjønn, Navn)
 print("Storing all the previous friend groups")
@@ -104,53 +96,52 @@ while True:
     child += 1
 
     # breaks out of the while loop when the next cell is empty
-    if not sheet2_infile.cell(row = (child + 1), column = 1).value:
+    if not sheet2_infile.cell(row=(child + 1), column=1).value:
         break
-    
-    # grabs the childs name and stores it in a variable
-    child_name = sheet2_infile.cell(row = (child + 1), column = 1).value
+
+    # grabs the child's name and stores it in a variable
+    child_name = sheet2_infile.cell(row=(child + 1), column=1).value
 
     # grab all the names of "has_been_in_group_with" (Har vært i gruppen med)
     while True:
-        
+
         # breaks out of the while loop when the next cell is empty
-        if not sheet2_infile.cell(row = (child + 1), column = iterator).value:
+        if not sheet2_infile.cell(row=(child + 1), column=iterator).value:
             iterator = 3
             break
-        
+
         # Grabs and stores the child names and stores in a list
-        has_been_in_group_with.append(sheet2_infile.cell(row = (child + 1), column = iterator).value)
+        has_been_in_group_with.append(sheet2_infile.cell(row=(child + 1), column=iterator).value)
         iterator += 1
-    
-    # After the list "has_been_in_group_with" is done, this list is added to the archief the main child
-    archief[child_name] = {"gender": sheet2_infile.cell(row = (child + 1), column = 2).value, "has_been_in_group_with": has_been_in_group_with}
-    
+
+    # After the list "has_been_in_group_with" is done, this list is added to the archive the main child
+    archive[child_name] = {"gender": sheet2_infile.cell(row=(child + 1), column=2).value,
+                           "has_been_in_group_with": has_been_in_group_with}
+
     # List is emptied for the next main child
     has_been_in_group_with = list()
-    
 
 #################################################################
 #                                                               #
-#       Add lastest groups to the "has_been_in_group_with"      #
+#       Add latest groups to the "has_been_in_group_with"       #
 #                                                               #
 #################################################################
 
 
 # Access each group from the latest groups
-print("Adding the latest groups to the archief")
+print("Adding the latest groups to the archive")
 for group in groups:
-    
-    #Access the names in the group
+
+    # Access the names in the group
     for name in groups[group]:
 
-        # Check if name is in the archief
-        if name in archief.keys():
-        
-            # Add the other children of the latest group into the archief
+        # Check if name is in the archive
+        if name in archive.keys():
+
+            # Add the other children of the latest group into the archive
             for kid in groups[group]:
                 if name != kid:
-                    archief[name]["has_been_in_group_with"].append(kid)
-        
+                    archive[name]["has_been_in_group_with"].append(kid)
 
 #################################################################
 #                                                               #
@@ -161,70 +152,70 @@ for group in groups:
 
 # Make a list containing all the names
 print("\nCreating new friend groups")
-randomized_list_of_names = list(archief.keys())
+randomized_list_of_names = list(archive.keys())
 
 
 # Function to randomize the order of the names in the list
-def randomizeChildren():
+def randomize_children():
     random.shuffle(randomized_list_of_names)
     return randomized_list_of_names
 
 
 # Initializing variables to make new friend groups
 total_nr_of_groups = 1
-nr_of_5_kids_per_group = len(archief)%4
-nr_of_4_kids_per_group = int((len(archief) - nr_of_5_kids_per_group * 5) / 4)
+nr_of_5_kids_per_group = len(archive) % 4
+nr_of_4_kids_per_group = int((len(archive) - nr_of_5_kids_per_group * 5) / 4)
 new_friend_groups = {}
 genders_per_group = {}
-tmp_genders = list() # empty list to hold the genders of the children in one group
 
 # Calculate how many groups are needed
-length_archief = len(archief)
-if length_archief >= 15:
-    total_nr_of_groups = math.floor(len(archief)/4)
+length_archive = len(archive)
+if length_archive >= 15:
+    total_nr_of_groups = math.floor(len(archive) / 4)
 else:
-    if length_archief % 4 <= 2:
-        total_nr_of_groups = math.floor(len(archief)/4)
+    if length_archive % 4 <= 2:
+        total_nr_of_groups = math.floor(len(archive) / 4)
     else:
-        total_nr_of_groups = math.ceil(len(archief)/4)
+        total_nr_of_groups = math.ceil(len(archive) / 4)
 
 
 # Function to make new groups from the randomized list
-def makeGroups():
-    randomizeChildren()
-    iterator = 1
+def make_groups():
+    randomize_children()
+    i = 1
 
     # Make the groups with 5 children
     for group_with_five in range(0, (nr_of_5_kids_per_group * 5), 5):
-        
         # Assigns the friend group number and store the names
-        new_friend_groups[iterator] = randomized_list_of_names[group_with_five:(group_with_five + 5)]
-        
+        new_friend_groups[i] = randomized_list_of_names[group_with_five:(group_with_five + 5)]
+
         # iterator + 1 for the next group (if any)
-        iterator += 1
-    
+        i += 1
+
     # Make the groups with 4 children
-    for group_with_four in range((nr_of_5_kids_per_group * 5), (nr_of_4_kids_per_group * 4 + nr_of_5_kids_per_group * 5), 4):
+    for group_with_four in range((nr_of_5_kids_per_group * 5),
+                                 (nr_of_4_kids_per_group * 4 + nr_of_5_kids_per_group * 5), 4):
         # Assigns the friend group number and store the names
-        new_friend_groups[iterator] = randomized_list_of_names[group_with_four:(group_with_four + 4)]
+        new_friend_groups[i] = randomized_list_of_names[group_with_four:(group_with_four + 4)]
 
         # iterator + 1 for the next group (if any)    
-        iterator += 1
+        i += 1
+
 
 # Function to make a list of all the genders in one group
-def genders():
-    makeGroups()
-    tmp_genders = list() # empty list to hold the genders of the children in one group
-    iterator = 0
+def list_of_genders():
+    make_groups()
+    tmp_genders = list()  # empty list to hold the genders of the children in one group
+    j = 0
 
-    for group in new_friend_groups:
-        for name in new_friend_groups[group]:
-            tmp_genders.append(archief[name]["gender"])
-            
-        genders_per_group[iterator] = tmp_genders
+    for group_number in new_friend_groups:
+        for name_in_group in new_friend_groups[group_number]:
+            tmp_genders.append(archive[name_in_group]["gender"])
+
+        genders_per_group[j] = tmp_genders
         tmp_genders = list()
-        iterator += 1
-    
+        j += 1
+
     return genders_per_group
 
 
@@ -232,19 +223,18 @@ def genders():
 #    5 points for same gender
 #   10 points for having been in same group before
 def scoring():
-    iterator = 1
-    for group in new_friend_groups:
-        print("\nVennegruppe", iterator, end=":  ")
-        iterator += 1
-        for child in new_friend_groups[group]:
-            print(child, end=", ")
+    k = 1
+    for group_scoring in new_friend_groups:
+        print("\nVennegruppe", k, end=":  ")
+        k += 1
+        for child_scoring in new_friend_groups[group_scoring]:
+            print(child_scoring, end=", ")
 
 
-genders()
-print("Total number of children: {0}\nTotal number of groups: {1} \n {2} groups with 5 children and," 
-      "\n {3} groups with 4 children".format(length_archief, nr_of_5_kids_per_group + nr_of_4_kids_per_group,
-      nr_of_5_kids_per_group, nr_of_4_kids_per_group))
-
+list_of_genders()
+print("Total number of children: {0}\nTotal number of groups: {1} \n {2} groups with 5 children and,"
+      "\n {3} groups with 4 children".format(length_archive, nr_of_5_kids_per_group + nr_of_4_kids_per_group,
+                                             nr_of_5_kids_per_group, nr_of_4_kids_per_group))
 
 # Print the new friends group to the screen
 iterator = 1
@@ -254,17 +244,16 @@ for group in new_friend_groups:
     for child in new_friend_groups[group]:
         print(child, end=", ")
 
-
 # Data can be assigned directly to cells
-#x = random.randint(1, 10)
-#ws['A1'] = x
+# x = random.randint(1, 10)
+# ws['A1'] = x
 
 # Save the file
-#wb.save("sample.xlsx")
+# wb.save("sample.xlsx")
 
 # Empty dictionaries
 groups = {}
-archief = {}
+archive = {}
 
 # Success
 sys.exit(0)
